c9bfd12b8810db1f5b59b0a743ca85731052027e
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-15 16:53:46 +0800

    set request using urlRequest

--- MilestoneProject_10_12/Views/ContentView.swift
+++ MilestoneProject_10_12/Views/ContentView.swift
@@ -14,10 +14,20 @@ struct ContentView: View {
     }
     
     func loadData() {
-        guard let url = "https://www.hackingwithswift.com/samples/friendface.json" else {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")  else {
             print("Invalid url")
             return
         }
+        
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(FriendFaces.self, from: data) {
+                    
+                }
+            }
+        }
+        
     }
 }
 
