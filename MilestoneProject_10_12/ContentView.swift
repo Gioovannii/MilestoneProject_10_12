495dea98d1868f8e42ecf19155fa3cd9cae047c5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-15 16:37:11 +0800

    Initial Commit

---
+++ MilestoneProject_10_12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MilestoneProject_10_12
+//
+//  Created by Giovanni Gaffé on 2021/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MilestoneProject_10_12/MilestoneProject_10_12App.swift
@@ -0,0 +1,17 @@
+//
+//  MilestoneProject_10_12App.swift
+//  MilestoneProject_10_12
+//
+//  Created by Giovanni Gaffé on 2021/11/15.
+//
+
+import SwiftUI
+
+@main
+struct MilestoneProject_10_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
