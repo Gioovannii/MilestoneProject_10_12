86a741648a857e2770036abc81090af60b644f1e
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-15 16:46:13 +0800

    Create struct for coding

--- MilestoneProject_10_12/FriendFaces.swift
+++ MilestoneProject_10_12/FriendFaces.swift
@@ -5,4 +5,22 @@
 //  Created by Giovanni Gaffé on 2021/11/15.
 //
 
+import Foundation
 
+// MARK: - FacesJSONElement
+
+struct FriendFaces: Codable {
+    let id: String
+    let isActive: Bool
+    let name: String
+    let age: Int
+    let company, email, address, about: String
+    let registered: Date
+    let tags: [String]
+    let friends: [Friend]
+}
+
+// MARK: - Friend
+struct Friend: Codable {
+    let id, name: String
+}
