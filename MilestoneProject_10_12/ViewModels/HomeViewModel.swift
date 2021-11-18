1bcca5c1edeae71a87fd4dd823aa14bd923cd7b1
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-18 16:44:11 +0800

    move code to view model to fetch data

--- MilestoneProject_10_12/ViewModels/HomeViewModel.swift
+++ MilestoneProject_10_12/ViewModels/HomeViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var users = [User]()
+    
+    func loadData() {
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid url")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode([User].self, from: data) {
+                    DispatchQueue.main.async {
+                        self.users = decodedResponse
+                        
+                    }
+                    return
+                }
+            }
+            print("Fetch failed \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
+    }
+}
