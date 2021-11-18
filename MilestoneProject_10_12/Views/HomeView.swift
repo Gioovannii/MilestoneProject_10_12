1350407f9843be87b657a5a1c48c08a2f6ec7bdd
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-18 16:44:40 +0800

    refactor home view

--- MilestoneProject_10_12/Views/HomeView.swift
+++ MilestoneProject_10_12/Views/HomeView.swift
@@ -8,46 +8,23 @@
 import SwiftUI
 
 struct HomeView: View {
-    
-    @State var users = [User]()
+    @ObservedObject var homeViewModel = HomeViewModel()
     
     var body: some View {
         NavigationView {
-            List(users, id: \.id) { user in
-                NavigationLink(destination: DetailView(user: user)) {
-                    HStack {
-                        Text(user.name)
-                        Spacer()
-                        Text("\(user.age) ans")
+            List(homeViewModel.users, id: \.id) { user in
+                NavigationLink(destination: DetailView(
+                    detailViewModel: DetailViewModel(user: user))) {
+                        HStack {
+                            Text(user.name)
+                            Spacer()
+                            Text("\(user.age) ans")
+                        }
                     }
-                }
             }
             .navigationTitle("User List")
-            .onAppear(perform: loadData)
-            
+            .onAppear(perform: homeViewModel.loadData)
         }
-    }
-    
-    
-    func loadData() {
-        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
-            print("Invalid url")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode([User].self, from: data) {
-                    DispatchQueue.main.async {
-                        self.users = decodedResponse
-                        
-                    }
-                    return
-                }
-            }
-            print("Fetch failed \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
     }
 }
 
