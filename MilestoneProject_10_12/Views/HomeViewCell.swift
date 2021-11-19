74a2133108f6a0e93c1432c97237d7634d1b3b70
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-19 08:20:22 +0800

    set a cell with an age and a name

---
+++ MilestoneProject_10_12/Views/HomeViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewCell.swift
+//  MilestoneProject_10_12
+//
+//  Created by Giovanni Gaffé on 2021/11/19.
+//
+
+import SwiftUI
+
+struct HomeViewCell: View {
+    let name: String
+    let age: Int
+    
+    var body: some View {
+        HStack {
+            Text(name)
+            Spacer()
+            Text("\(age) ans")
+        }
+    }
+}
+
+struct HomeViewCell_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewCell(name: "Giovanni", age: 31)
+    }
+}
