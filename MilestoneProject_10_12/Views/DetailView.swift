4c06d16170fd9705b53baee1364966701cc64f1f
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-19 10:21:13 +0800

    update detail view to show users friend

--- MilestoneProject_10_12/Views/DetailView.swift
+++ MilestoneProject_10_12/Views/DetailView.swift
@@ -12,33 +12,62 @@ struct DetailView: View {
     @ObservedObject var viewModel: DetailViewModel
     
     var body: some View {
-        ZStack {
-            NavigationView {
-                
-                let user = viewModel.user
-                VStack {
-                    
-                    Text("I work for \(user.company)")
-                    Text("My e-mail is \(user.email)")
-                    Text("My address is \(user.address)")
-                    Text("I registered  \(user.registered)")
-                    
-                    Text(viewModel.user.tags.joined(separator: ", "))
-                        .padding()
-                    
-                    VStack {
-                        Text("A small description of me \(user.about)")
-                            .font(.headline)
-                        Text(user.about)
-                            .padding()
-                            .background(Color.green)
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                    }
-                }
+        
+        let user = viewModel.user
+        Form {
+            HStack {
+                Text("I work for")
+                    .fontWeight(.semibold)
+                Text(user.company)
             }
-            .navigationTitle(viewModel.user.name)
-            .navigationBarTitleDisplayMode(.inline)
+            HStack {
+                Text("My e-mail is")
+                    .fontWeight(.semibold)
+                Text(user.email)
+            }
+            
+            HStack {
+                Text("My address is")
+                    .fontWeight(.semibold)
+                Text(user.address)
+            }
+            
+            HStack {
+                Text("I registered")
+                    .fontWeight(.semibold)
+                Text(user.registered)
+            }
+            HStack {
+                Text("My friends :")
+                    .fontWeight(.semibold)
+                Text(getFriends())
+            }
+            HStack {
+                Text("Here is my tags ")
+                    .fontWeight(.semibold)
+                Text(user.tags.joined(separator: ", "))
+            }
+            
+            VStack {
+                Text("A small description of me")
+                    .font(.headline)
+                Text(user.about)
+                    .padding()
+                    .background(Color.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
         }
+        .font(.body)
+        .navigationTitle(viewModel.user.name)
+    }
+    
+    func getFriends() -> String {
+        var friends = [String]()
+        
+        for user in viewModel.user.friends {
+            friends.append(user.name)
+        }
+        return friends.joined(separator: ", ")
     }
 }
 
