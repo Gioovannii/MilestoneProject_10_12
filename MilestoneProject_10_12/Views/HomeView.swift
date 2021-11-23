ea3abd82ce98a1dfb5fa46f0ac41c66314b456e8
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-23 10:36:03 +0800

    use view model to save data

--- MilestoneProject_10_12/Views/HomeView.swift
+++ MilestoneProject_10_12/Views/HomeView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     @ObservedObject var viewModel = HomeViewModel()
+    @Environment(\.managedObjectContext) var moc
+    //    @FetchRequest(entity: User.entity(), sortDescriptors: []) var users: FetchedResults<User>
     
     var body: some View {
         NavigationView {
             List(viewModel.users, id: \.id) { user in
+                //            List(users, id: \.self) { user in
                 NavigationLink(destination: DetailView(
                     viewModel: DetailViewModel(user: user))) {
                         HomeViewCell(name: user.name, age: user.age)
@@ -20,7 +23,22 @@ struct HomeView: View {
             }
             .navigationTitle("User List")
             .onAppear(perform: viewModel.loadData)
+            .onAppear(perform: saveUsers)
         }
+    }
+    
+    func saveUsers() {
+        
+        for user in viewModel.users {
+            let userRepresentable = User(context: moc)
+            userRepresentable.id = user.id
+            userRepresentable.isActive = user.isActive
+            userRepresentable.id = user.id
+            userRepresentable.registered = user.registered
+            userRepresentable.tags = user.tags
+            userRepresentable.friends = user.friends as? [Friend]
+        }
+        try? self.moc.save()
     }
 }
 
