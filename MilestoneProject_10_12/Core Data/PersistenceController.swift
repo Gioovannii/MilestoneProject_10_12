db29c4a6da5c90025c1953f414d4aac43795cfd0
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-23 10:33:36 +0800

    Add persistent controller

---
+++ MilestoneProject_10_12/Core Data/PersistenceController.swift
@@ -0,0 +1,61 @@
+//
+//  PersistenceController.swift
+//  MilestoneProject_10_12
+//
+//  Created by Giovanni Gaffé on 2021/11/19.
+//
+
+import Foundation
+import CoreData
+
+struct PersistenceController {
+    // A singleton for our entire app to use
+    static let shared = PersistenceController()
+
+    // Storage for Core Data
+    let container: NSPersistentContainer
+
+    // A test configuration for SwiftUI previews
+    static var preview: PersistenceController = {
+        let controller = PersistenceController(inMemory: true)
+
+        // Create 10 example programming languages.
+        for _ in 0..<10 {
+            let userPreview = User(context: controller.container.viewContext)
+            userPreview.name = "Giovanni"
+            userPreview.id = "592578d8-e823-4b9e-ab42-d654281dc8e9"
+        }
+
+        return controller
+    }()
+
+    // An initializer to load Core Data, optionally able
+    // to use an in-memory store.
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "Model")
+
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // MARK: - Save 
+
+    func save() {
+        let context = container.viewContext
+
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Show some error here
+            }
+        }
+    }
+}
