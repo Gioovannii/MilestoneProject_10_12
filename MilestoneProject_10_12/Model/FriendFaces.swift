dc8945c713a4ac19d19bad0f9f4f41cce91747be
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-23 10:33:55 +0800

    Set an user Representable to decode data

--- MilestoneProject_10_12/Model/FriendFaces.swift
+++ MilestoneProject_10_12/Model/FriendFaces.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // MARK: - FacesJSONElement
 
-struct User: Codable, Identifiable {
+struct UserRepresentable: Codable, Identifiable {
     let id: String
     let isActive: Bool
     let name: String
@@ -17,18 +17,17 @@ struct User: Codable, Identifiable {
     let company, email, address, about: String
     let registered: String
     let tags: [String]
-    let friends: [Friend]
-    
+    let friends: [FriendRepresentable]
 }
 
 // MARK: - Friend
-struct Friend: Codable {
+struct FriendRepresentable: Codable {
     let id, name: String
 }
 
 
-extension User {
-    static let dummyUser = User(
+extension UserRepresentable {
+    static let dummyUser = UserRepresentable(
         id: UUID().uuidString, isActive: true,
         name: "Giovanni", age: 31,
         company: "Radiantix", email: "gaffejonathan@ymail.com",
@@ -42,8 +41,8 @@ extension User {
                "eu",
                "labore",
                "qui"],
-        friends: [Friend(id: "50a48fa3-2c0f-4397-ac50-64da464f9954", name: "Alford Rodriguez"),
-                  Friend(id: "e8ad0264-aaec-436c-ad6c-26b8fe171245", name: "Chase Jensen"),
-                  Friend(id: "e8b97347-d6e3-421d-ae7f-d6c6bc8626f3", name: "Leach Walls"),
-                  Friend(id: "54de729e-1730-4ad6-8452-a95c566460f4", name: "Maureen Larsen")])
+        friends: [FriendRepresentable(id: "50a48fa3-2c0f-4397-ac50-64da464f9954", name: "Alford Rodriguez"),
+                  FriendRepresentable(id: "e8ad0264-aaec-436c-ad6c-26b8fe171245", name: "Chase Jensen"),
+                  FriendRepresentable(id: "e8b97347-d6e3-421d-ae7f-d6c6bc8626f3", name: "Leach Walls"),
+                  FriendRepresentable(id: "54de729e-1730-4ad6-8452-a95c566460f4", name: "Maureen Larsen")])
 }
