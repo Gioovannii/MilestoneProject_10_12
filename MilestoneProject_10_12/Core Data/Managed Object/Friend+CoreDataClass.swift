378353c9e6fe822840a2bcc94acf868bc8d7b17f
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-23 10:31:21 +0800

    Add ns managed object

---
+++ MilestoneProject_10_12/Core Data/Managed Object/Friend+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Friend+CoreDataClass.swift
+//  MilestoneProject_10_12
+//
+//  Created by Giovanni Gaffé on 2021/11/19.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Friend: NSManagedObject {
+
+}
