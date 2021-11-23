5dc3aa490d844a01300cca12f5adb38389ca7a61
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-11-23 10:32:46 +0800

    Add managed object

---
+++ MilestoneProject_10_12/Core Data/Managed Object/User+CoreDataProperties.swift
@@ -0,0 +1,37 @@
+//
+//  User+CoreDataProperties.swift
+//  MilestoneProject_10_12
+//
+//  Created by Giovanni Gaffé on 2021/11/19.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var about: String?
+    @NSManaged public var address: String?
+    @NSManaged public var age: Int16
+    @NSManaged public var company: String?
+    @NSManaged public var email: String?
+    @NSManaged public var friends: [Friend]?
+    @NSManaged public var id: String?
+    @NSManaged public var isActive: Bool
+    @NSManaged public var name: String?
+    @NSManaged public var registered: String?
+    @NSManaged public var tags: [String]?
+    
+    
+
+}
+
+extension User : Identifiable {
+
+}
